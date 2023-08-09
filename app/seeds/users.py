2864a0d7de8c312f469d5ab484509f4ddcccf2a8
James <james.hernandez1225@gmail.com>
2023-08-09 15:34:55 -0400

    fixing seeder data

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -10,16 +10,16 @@ def seed_users():
         username='james', email='james@aa.io', password='password', image="https://i.imgur.com/Ni48C6r.jpeg")
     bobbie = User(
         username='bobbie', email='bobbie@aa.io', password='password')
-    user3 =User(username='Demo2', email='demo2@aa.io', password='password'),
-    user4 =User(username='JohnDoe', email='john.doe@example.com', password='securepassword'),
-    user5 =User(username='JaneSmith', email='jane.smith@example.com', password='strongpassword'),
-    user6 = User(username='MikeJohnson', email='mike.johnson@example.com', password='mysecretpass'),
-    user7 = User(username='EmilyBrown', email='emily.brown@example.com', password='safepassword123'),
-    user8 = User(username='RobertWilliams', email='robert.williams@example.com', password='mycoolpassword'),
-    user9 = User(username='SarahLee', email='sarah.lee@example.com', password='pass1234'),
-    user10 = User(username='ChrisTaylor', email='chris.taylor@example.com', password='mysecurepass'),
-    user11 = User(username='JenniferDavis', email='jennifer.davis@example.com', password='myp@ssword'),
-    user12 = User(username='DanielMiller', email='daniel.miller@example.com', password='password321'),
+    user3 =User(username='Demo2', email='demo2@aa.io', password='password')
+    user4 =User(username='JohnDoe', email='john.doe@example.com', password='securepassword')
+    user5 =User(username='JaneSmith', email='jane.smith@example.com', password='strongpassword')
+    user6 = User(username='MikeJohnson', email='mike.johnson@example.com', password='mysecretpass')
+    user7 = User(username='EmilyBrown', email='emily.brown@example.com', password='safepassword123')
+    user8 = User(username='RobertWilliams', email='robert.williams@example.com', password='mycoolpassword')
+    user9 = User(username='SarahLee', email='sarah.lee@example.com', password='pass1234')
+    user10 = User(username='ChrisTaylor', email='chris.taylor@example.com', password='mysecurepass')
+    user11 = User(username='JenniferDavis', email='jennifer.davis@example.com', password='myp@ssword')
+    user12 = User(username='DanielMiller', email='daniel.miller@example.com', password='password321')
     user13 = User(username='LindaWilson', email='linda.wilson@example.com', password='pass4me')
 
     [db.session.add(newUser) for newUser in [demo,marnie,bobbie, user3,user4,user5,user6,user7,user8,user9,user10,user11,user12,user13]]
