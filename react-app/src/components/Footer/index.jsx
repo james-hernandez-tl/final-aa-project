5f08b6f04439608b0602babf1cde2e873a50b87e
James <james.hernandez1225@gmail.com>
2023-08-15 16:43:47 -0400

    personal link will now redirect to a differnt tab

--- react-app/src/components/Footer/index.jsx
+++ react-app/src/components/Footer/index.jsx
@@ -4,16 +4,19 @@ export default function Footer() {
   return (
     <div className="Footer">
       <div className="Footer-icon-wrapper">
-        <a href="https://github.com/ihavenoide">
+        <a target="_blank" href="https://github.com/ihavenoide">
           <i className="fa-brands fa-github footer-icon"></i>
         </a>
-        <a href="https://www.linkedin.com/in/james-hernandez-76191623a/">
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/james-hernandez-76191623a/"
+        >
           <i className="fa-brands fa-linkedin"></i>
         </a>
-        <a href="https://wellfound.com/u/james-hernandez-15">
+        <a target="_blank" href="https://wellfound.com/u/james-hernandez-15">
           <i className="fa-brands fa-angellist"></i>
         </a>
-        <a href="https://ihavenoide.github.io/">
+        <a target="_blank" href="https://ihavenoide.github.io/">
           <i className="fa-solid fa-suitcase"></i>
         </a>
       </div>
