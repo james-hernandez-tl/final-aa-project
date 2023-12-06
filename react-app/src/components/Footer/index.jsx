46743b0d657d2f34bbed1857c90e5821acf0c1a2
James <james.hernandez1225@gmail.com>
2023-12-06 15:13:33 -0500

    updated github link

--- react-app/src/components/Footer/index.jsx
+++ react-app/src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div id="Footer" className="Footer">
       <div className="Footer-title">Get to know about me!</div>
       <div className="Footer-icon-wrapper">
-        <a target="_blank" href="https://github.com/ihavenoide">
+        <a target="_blank" href="https://github.com/james-hernandez-tl">
           <i className="fa-brands fa-github footer-icon"></i>
         </a>
         <a
@@ -17,7 +17,7 @@ export default function Footer() {
         <a target="_blank" href="https://wellfound.com/u/james-hernandez-15">
           <i className="fa-brands fa-angellist"></i>
         </a>
-        <a target="_blank" href="https://ihavenoide.github.io/">
+        <a target="_blank" href="https://james-hernandez-tl.github.io/">
           <i className="fa-solid fa-suitcase"></i>
         </a>
       </div>
